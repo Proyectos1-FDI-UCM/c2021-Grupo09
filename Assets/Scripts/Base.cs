cdcda07cf18cd4aa3b4b83e71687c3a63a96409b
Jagomeze <jagomeze@ucm.es>
2021-04-30 12:44:12 +0200

    Como enemigo quiero hacer daño a la base al tocarla y desaparecer.

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+    public int vidaBase;
+    public int dañoRecibido;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //especificamos que la colision sea con un enemigo
+        if (collision.gameObject.GetComponent<Enemy>())
+        {
+            vidaBase -= dañoRecibido;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //especificamos que la colision sea con la base
+
+        if (collision.gameObject.GetComponen<Base>())
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
