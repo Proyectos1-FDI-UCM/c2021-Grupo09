b9a717ae4c9b01106dacf8008c256e7e5961f923
ARodulfo <72988547+ARodulfo@users.noreply.github.com>
2021-05-10 13:56:17 +0200

    Menú de opciones implementado(sonido y pantalla completa)

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetMUSICVolume(float volume)
+    {
+        audioMixer.SetFloat("music", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("sfx", volume);
+    }
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
