5ee8d595c0df33394220bff3e859930b4c1b3a92
ARodulfo <72988547+ARodulfo@users.noreply.github.com>
2021-05-25 04:21:39 +0200

    Info de torres al pasar el raton por encima.

---
+++ Assets/Scripts/MouseHovering.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseHovering : MonoBehaviour
+{
+    public GameObject enableWhenHovering;
+    void OnMouseOver()
+    {
+        if (Time.timeScale != 0f) {
+            //If your mouse hovers over the GameObject with the script attached, output this message
+            enableWhenHovering.SetActive(true);
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if(Time.timeScale != 0f)
+        {
+           //The mouse is no longer hovering over the GameObject so output this message each frame
+           enableWhenHovering.SetActive(false);
+        }  
+    }
+}
